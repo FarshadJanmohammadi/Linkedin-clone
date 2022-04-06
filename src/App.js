6af77f1f7ee0d3394cf0614f32e1ed0cfa765422
Farshad <FarshadJanmohammadi@gmail.com>
2022-04-07 00:18:17 +0430

    end of header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Components/Login";
+import Home from "./Components/Home";
 
 import "./App.css";
-import Login from "./Components/Login";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </Router>
     </div>
